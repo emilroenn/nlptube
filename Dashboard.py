7a6e263a2ed36368b41012995c927e6df815abb3
CogSciDamgaard <lasklat@hotmail.com>
2021-06-02 17:31:51 +0200

    lots_of_nice_stuffs

--- Dashboard.py
+++ Dashboard.py
@@ -8,9 +8,9 @@ from nltk.corpus import stopwords
 import gensim
 from gensim.utils import simple_preprocess
 import spacy
-import re
+import re 
 import os #
-import pickle
+import pickle 
 import pandas as pd
 from pandas import DataFrame
 import google.oauth2.credentials
@@ -26,23 +26,49 @@ from stqdm import stqdm
 from wordcloud import WordCloud
 from PIL import Image
 
+from enum import Enum
+from io import BytesIO, StringIO
+from typing import Union
+
+import pandas as pd
+import streamlit as st
+
 
 
 
 #### POSSIBLE FUNCTIONS TO MOVE ####
 
-def pipeline2 (df):
-    progress = st.text('2/4: Running Term-Frequency Inverse-Document-Frequency analysis...')
-    df = tfidf_prep(df)
-    progress.text('3/4: Vectorizing documents...')
-    df = vectorize(df)
-    progress.text('4/4: Building wordcloud visualization...')
-    wordcloud = visualize(df)
+def pipeline_multiple (df_list, cloud_color, cloud_bg, cloud_shape, cloud_font):
+    progress = st.text('1/3: Running Term-Frequency Inverse-Document-Frequency analysis...')
+    total_dfs = len(df_list)
+    df_list = tfidf_prep(df_list)
+    progress.text('2/3: Vectorizing documents...')
+    df = vectorize_multiple(df_list)
+    progress.text('3/3: Building wordcloud visualization...')
+    wordcloud_list = []
+    for x in range(total_dfs):
+        selected_df = df[[x]]
+        wordcloud = visualize(selected_df, cloud_color, cloud_bg, cloud_shape, cloud_font, x)
+        wordcloud_list.append(wordcloud)
+    progress.text('Done!')
+    return wordcloud_list
+
+def pipeline_single (df, cloud_color, cloud_bg, cloud_shape, cloud_font):
+    progress = st.text('1/2: Vectorizing documents...')
+ #   df = tfidf_prep(df)
+  #  progress.text('3/4: Vectorizing documents...')
+    df = vectorize_single(df)
+    progress.text('2/2: Building wordcloud visualization...')
+    wordcloud = visualize(df, cloud_color, cloud_bg, cloud_shape, cloud_font)
+    progress.text('Done!')
     return wordcloud
 
 
 
-ss = SessionState.get(current = "Welcome", data1q = "NA", data1s = 0, data2q = "NA", data2s = 0, data3q = "NA", data3s = 0)
+ss = SessionState.get(current = "Welcome", 
+                data1q = "NA", data1s = 0, data1tf = "NA",
+                data2q = "NA", data2s = 0, data2tf = "NA", 
+                data3q = "NA", data3s = 0, data3tf = "NA")
 #ss.ticker = 0
 #ss.x = 
 #ss.current = "Main"
@@ -121,40 +147,93 @@ def page_how():
 
 def page_upload():
     st.title('Upload your own motherfucking data')
-    dataset = st.selectbox("Select a dataset to create", ['Data 1', 'Data 2', 'Data 3'])
+    st.info("Select the data container that you wish to upload your data to")
+    dataset = st.selectbox("",['Data 1', 'Data 2', 'Data 3'])
 
-    upload_button = st.button('Upload')
+    STYLE = """
+    <style>
+    img {
+        max-width: 100%;
+    }
+    </style>
+    """
 
-    if upload_button:
-        if dataset == "Data 1":
-            ss.data1 = get_data(user_input1, user_input2)
-            ss.data1q = user_input1
-            ss.data1s = len(ss.data1)
-            total = len(ss.data1)
-            csv = ss.data1.to_csv(index=False)
-            b64 = base64.b64encode(csv.encode()).decode()  # some strings <-> bytes conversions necessary here
-            ss.data1_href = f'<a href="data:file/csv;base64,{b64}">Download CSV File</a> (right-click and save as &lt;some_name&gt;.csv)'
-            st.markdown(ss.data1_href, unsafe_allow_html=True)
-        if dataset == "Data 2":
-            ss.data2 = get_data(user_input1, user_input2)
-            ss.data2q = user_input1
-            ss.data2s = len(ss.data2)
-            total = len(ss.data2)
-            csv = ss.data2.to_csv(index=False)
-            b64 = base64.b64encode(csv.encode()).decode()  # some strings <-> bytes conversions necessary here
-            ss.data2_href = f'<a href="data:file/csv;base64,{b64}">Download CSV File</a> (right-click and save as &lt;some_name&gt;.csv)'
-            st.markdown(ss.data2_href, unsafe_allow_html=True)
-        if dataset == "Data 3":
-            ss.data3 = get_data(user_input1, user_input2)
-            ss.data3q = user_input1
-            ss.data3s = len(ss.data3)
-            total = len(ss.data3)
-            csv = ss.data3.to_csv(index=False)
-            b64 = base64.b64encode(csv.encode()).decode()  # some strings <-> bytes conversions necessary here
-            ss.data3_href = f'<a href="data:file/csv;base64,{b64}">Download CSV File</a> (right-click and save as &lt;some_name&gt;.csv)'
-            st.markdown(ss.data3_href, unsafe_allow_html=True)
+    FILE_TYPES = ["csv", "py", "png", "jpg"]
 
-        st.write("Data uploaded!")
+
+    class FileType(Enum):
+        """Used to distinguish between file types"""
+
+        IMAGE = "Image"
+        CSV = "csv"
+        PYTHON = "Python"
+
+    def get_file_type(file: Union[BytesIO, StringIO]) -> FileType:
+        """The file uploader widget does not provide information on the type of file uploaded so we have
+        to guess using rules or ML. See
+        [Issue 896](https://github.com/streamlit/streamlit/issues/896)
+
+        I've implemented rules for now :-)
+
+        Arguments:
+            file {Union[BytesIO, StringIO]} -- The file uploaded
+
+        Returns:
+            FileType -- A best guess of the file type
+        """
+
+        return FileType.CSV
+
+    def main2():
+        """Run this function to display the Streamlit app"""
+        st.markdown(STYLE, unsafe_allow_html=True)
+
+        file = st.file_uploader("Upload file", type="CSV")
+        show_file = st.empty()
+        if not file:
+            show_file.info("Please upload a file of type: CSV " )
+            return
+
+        file_type = get_file_type(file)
+        # if file_type == FileType.IMAGE:
+            # st.info("please upload a .csv file in the scraper format, and not an image")
+        # elif file_type == FileType.PYTHON:
+            # st.info("please upload a .csv file in the scraper format, and not and not python code")
+        if file_type == FileType.CSV:
+            if dataset == "Data 1":
+                data = pd.read_csv(file)
+                ss.data1 = data
+                ss.data1q = data.at[2,'Query']
+                ss.data1s = len(ss.data1)
+                csv = ss.data1.to_csv(index=False)
+                b64 = base64.b64encode(csv.encode()).decode()  # some strings <-> bytes conversions necessary here
+                ss.data1_href = f'<a href="data:file/csv;base64,{b64}">Download CSV File</a> (right-click and save as &lt;some_name&gt;.csv)'
+                st.info(f"file using the query '{ss.data1q}' with {ss.data1s} comments successfully uploaded to data container 1")
+                st.dataframe(data.head(10))
+            if dataset == "Data 2":
+                data = pd.read_csv(file)
+                ss.data2 = data
+                ss.data2q = data.at[2,'Query']
+                ss.data2s = len(ss.data2)
+                csv = ss.data2.to_csv(index=False)
+                b64 = base64.b64encode(csv.encode()).decode()  # some strings <-> bytes conversions necessary here
+                ss.data2_href = f'<a href="data:file/csv;base64,{b64}">Download CSV File</a> (right-click and save as &lt;some_name&gt;.csv)'
+                st.info(f"file using the query '{ss.data2q}' with {ss.data2s} comments successfully uploaded to data container 2")
+                st.dataframe(data.head(10))
+            if dataset == "Data 3":
+                data = pd.read_csv(file)
+                ss.data3 = data
+                ss.data3q = data.at[2,'Query']
+                ss.data3s = len(ss.data3)
+                csv = ss.data3.to_csv(index=False)
+                b64 = base64.b64encode(csv.encode()).decode()  # some strings <-> bytes conversions necessary here
+                ss.data3_href = f'<a href="data:file/csv;base64,{b64}">Download CSV File</a> (right-click and save as &lt;some_name&gt;.csv)'
+                st.info(f"file using the query '{ss.data3q}' with {ss.data3s} comments successfully uploaded to data container 3")
+                st.dataframe(data.head(10))
+
+        file.close()
+        
+    main2()
 
     
 def page_scrape():
@@ -207,21 +286,69 @@ def page_pre():
 def page_visualize():
     st.title('LETS GET SOME VISUALS UP IN HERE')
     st.text("This is where we visualize shit")   
-    dataset = st.selectbox("Select a dataset to visualize:", ['Data 1', 'Data 2', 'Data 3'])
 
-    word_cloud_button = st.button('Create Wordcloud')
+    type = st.selectbox("Analysis:", ['Individual datasets', 'Differences between datasets'])
 
-    try:
-        if word_cloud_button:
-            if dataset == "Data 1":
-                wordcloud = pipeline2(ss.data1)
-            if dataset == "Data 2":
-                wordcloud = pipeline2(ss.data2)
-            if dataset == "Data 3":
-                wordcloud = pipeline2(ss.data3)
-            st.image(wordcloud.to_array())
-    except AttributeError:
-        st.write("Please scrape some data first, fool!")
+    if type == "Individual datasets":
+        with st.form(key='my_form'):
+            dataset = st.selectbox("Select a dataset to visualize:", ['Data 1', 'Data 2', 'Data 3'])
+            st.text("Customize wordcloud:")   
+            col1, col2 = st.beta_columns(2)
+            cloud_color = col1.selectbox("Theme:", ['Default (Black)','summer', 'Wistia', 'OrRd', 'YlGn'])
+            cloud_font = col1.selectbox("Font:", ['Default (AU Passata)','AU', 'SpicyRice'])
+            cloud_bg = col2.selectbox("Background:", ['Default (White)','black', 'white', 'red'])
+            cloud_shape = col2.selectbox("Shape:", ['Default (Square)','Circle', 'Heart'])
+            submit_button = st.form_submit_button(label='Create Wordcloud')
+
+        try:
+            if submit_button:
+                if dataset == "Data 1":
+                    wordcloud = pipeline_single(ss.data1, cloud_color, cloud_bg, cloud_shape, cloud_font)
+                if dataset == "Data 2":
+                    wordcloud = pipeline_single(ss.data2, cloud_color, cloud_bg, cloud_shape, cloud_font)
+                if dataset == "Data 3":
+                    wordcloud = pipeline_single(ss.data3, cloud_color, cloud_bg, cloud_shape, cloud_font)
+                st.image(wordcloud.to_array())
+        except AttributeError as e:
+            st.write("Please scrape some data first, fool!")
+            st.write("Error:", e)
+        
+    
+    if type == "Differences between datasets":
+        with st.form(key='my_form'):
+        #    dataset = st.selectbox("Select a dataset to visualize:", ['Data 1', 'Data 2', 'Data 3'])
+            st.text("Customize wordcloud:")   
+            col1, col2 = st.beta_columns(2)
+            cloud_color = col1.selectbox("Theme:", ['Default (Black)','summer', 'Wistia', 'OrRd', 'YlGn'])
+            cloud_font = col1.selectbox("Font:", ['Default (AU Passata)','AU', 'SpicyRice'])
+            cloud_bg = col2.selectbox("Background:", ['Default (White)','black', 'white', 'red'])
+            cloud_shape = col2.selectbox("Shape:", ['Default (Square)','Circle', 'Heart'])
+            submit_button = st.form_submit_button(label='Create Wordcloud')
+
+        try:
+            if submit_button:
+                df_list = []
+                if hasattr(ss, 'data1'):
+                    df_list.append(ss.data1)
+                if hasattr(ss, 'data2'):
+                    df_list.append(ss.data2)
+                if hasattr(ss, 'data3'):
+                    df_list.append(ss.data3)
+                wordcloud_list = pipeline_multiple(df_list, cloud_color, cloud_bg, cloud_shape, cloud_font)
+                
+
+              #  with st.form(key='my_form'):
+              #      st.text("Customize wordcloud:")   
+              #      col1, col2 = st.beta_columns(2)
+                st.image(wordcloud_list[0].to_array())
+                st.image(wordcloud_list[1].to_array())
+             #   st.image(wordcloud_list[2].to_array())
+
+        except AttributeError as e:
+            st.write("Please scrape some data first, fool!")
+            st.write("Error:", e)
+        
+        
 
 def page_about():
     st.title('Fucking Fancy Youtube Scraper (TM)')
--- nlp_fns.py
+++ nlp_fns.py
@@ -17,7 +17,7 @@ from sklearn.feature_extraction.text import TfidfVectorizer
 import matplotlib.pyplot as plt
 from nlp_fns import * 
 
-
+import numpy as np
 from stqdm import stqdm
 from wordcloud import WordCloud
 from PIL import Image
@@ -42,11 +42,65 @@ def lemmatization(texts, allowed_postags=['NOUN', 'ADJ', 'VERB', 'ADV']):
 def make_bigrams(texts):
     return [bigram_mod[doc] for doc in texts]
 
-def tfidf_prep(document):
-    def make_bigrams(texts):
-        return [bigram_mod[doc] for doc in texts]
+
+
+def vectorize(df):
+    print("Running TF-IDF on all data sets...")
     #prep stopwords
-    more_stopwords = ("de, much, dont, im, liking, getting, gotten, ever, always, every, many, few, even, got, get, make, made, let, makes, im, say, said, better, best, really, would, something, came,much, oh, put, also, lyrics, lyric, comment, comments, lot, 2020, 2019, 2018, hey, hi, may, making, many, cause,com, youtube, etc, else, since, comes, come, www, la,que, re, el, use, used, still, much, ever, every, around,also, da, finally, sure, literal, literally, ooh, ya, every, album, let, -PRON-, pron, http, https").split(",")
+    more_stopwords = ("com, youtube, www, http, https").split(",")
+    stops = list(stopwords.words('english'))
+    stops.extend(more_stopwords)
+    stops = [x.strip(' ') for x in stops]
+    vectorizer = TfidfVectorizer(stop_words = stops, lowercase = True)#, tokenizer=LemmaTokenizer())
+    vectors = vectorizer.fit_transform([df])
+    feature_names = vectorizer.get_feature_names()
+    dense = vectors.todense()
+    denselist = dense.tolist()
+    df = pd.DataFrame(denselist, columns=feature_names)
+    #Clean results
+    df = df.T
+   # df = df.rename(columns = {0: 'word_word'}, inplace = False)
+    return df
+
+def visualize(df, cloud_color = "gist_gray", cloud_bg = "white",  cloud_shape = None, cloud_font = "AU", column = 0):
+    if cloud_color == 'Default (Black)':
+        cloud_color = "copper"
+    if cloud_bg == 'Default (White)':
+        cloud_bg = "white"
+    if cloud_font == 'Default (AU Passata)':
+        cloud_font = 'AU'
+    if cloud_shape == 'Default (Square)':
+        cloud_shape = None
+
+    cloud_scale = 0             # Scale word size mainly by rank (0) to frequency (1) 
+    cloud_horizontal = 1        # Ratio of word angles from horizontal (1) to vertical (0) 
+    
+    if cloud_shape != None:
+        cloud_shape = np.array(Image.open("./resources/shapes/" +cloud_shape + ".png"))
+    cloud_font = "./resources/fonts/" + cloud_font + ".ttf"
+    keys = tuple(df.index.tolist())
+    values = tuple(df[column])
+    my_dict = dict(zip(keys,values))
+
+    wordcloud = WordCloud(font_path = cloud_font, mask = cloud_shape, regexp=None, relative_scaling=cloud_scale, prefer_horizontal=cloud_horizontal, width = 800 , height = 800, background_color=cloud_bg, max_words=500, contour_width=0, colormap = cloud_color)
+
+    wordcloud.generate_from_frequencies(my_dict)
+
+    return wordcloud
+
+
+
+
+
+
+
+
+
+
+
+def vectorize_single(document):
+
+    more_stopwords = ("com, youtube, www, http, https").split(",")
     stops = list(stopwords.words('english'))
     stops.extend(more_stopwords)
     stops = [x.strip(' ') for x in stops]
@@ -64,49 +118,85 @@ def tfidf_prep(document):
     #Run stopword removal:
     document = [[word for word in simple_preprocess(str(doc)) if word not in set(stops)] for doc in document]
     #Run lemmatization:
-    document = lemmatization(document, allowed_postags=['NOUN', 'ADJ', 'VERB', 'ADV'])
+########  document = lemmatization(document, allowed_postags=['NOUN', 'ADJ', 'VERB', 'ADV'])
     #Remove weird tags:
     document = [[word for word in simple_preprocess(str(doc)) if word not in set(stops)] for doc in document]
     #Bigrams:
-    bigram = gensim.models.Phrases(document, min_count=10, threshold=30)
-    bigram_mod = gensim.models.phrases.Phraser(bigram)
-    document = make_bigrams(document)
+  #  bigram = gensim.models.Phrases(document, min_count=10, threshold=30)
+  #  bigram_mod = gensim.models.phrases.Phraser(bigram)
+  #  document = make_bigrams(document)
     #Smash to string:
     document = [' '.join(sent) for sent in document]
     document = ' '.join(document)
+
+    # Vectorizing
+    vectorizer = TfidfVectorizer(stop_words = stops, lowercase = True)#, tokenizer=LemmaTokenizer())
+    vectors = vectorizer.fit_transform([document])
+    feature_names = vectorizer.get_feature_names()
+    dense = vectors.todense()
+    denselist = dense.tolist()
+    document = pd.DataFrame(denselist, columns=feature_names)
+    #Clean results
+    document = document.T
+  #  document = document.rename(columns = {0: 'word_word'}, inplace = False)
     return document
 
 
-def vectorize(df):
+def vectorize_multiple(df_list):
     print("Running TF-IDF on all data sets...")
     #prep stopwords
-    more_stopwords = ("de, much, dont, im, liking, getting, gotten, ever, always, every, many, few, even, got, get, make, made, let, makes, im, say, said, better, best, really, would, something, came,much, oh, put, also, lyrics, lyric, comment, comments, lot, 2020, 2019, 2018, hey, hi, may, making, many, cause,com, youtube, etc, else, since, comes, come, www, la,que, re, el, use, used, still, much, ever, every, around,also, da, finally, sure, literal, literally, ooh, ya, every, album, let, -PRON-, pron, http, https").split(",")
+    more_stopwords = ("com, youtube, www, http, https").split(",")
     stops = list(stopwords.words('english'))
     stops.extend(more_stopwords)
     stops = [x.strip(' ') for x in stops]
     vectorizer = TfidfVectorizer(stop_words = stops, lowercase = True)#, tokenizer=LemmaTokenizer())
-    vectors = vectorizer.fit_transform([df])
+    vectors = vectorizer.fit_transform(df_list)
+
     feature_names = vectorizer.get_feature_names()
     dense = vectors.todense()
     denselist = dense.tolist()
     df = pd.DataFrame(denselist, columns=feature_names)
     #Clean results
     df = df.T
-    df = df.rename(columns = {0: 'word_word'}, inplace = False)
+  #  df = df.rename(columns = {0: 'doc_1', 1: 'doc_2'}, inplace = False)
     return df
 
-def visualize(df):
-    cloud_color = "summer"     #summer # Set wordcloud color scheme ('Wistia' or 'OrRd' are nice! YlGn is great for ayahuasca!)
-    cloud_bg_color = "black"    # Set background color ("#182a2a" is a nice, dark green; #2b2d2f coal grey!)
-    cloud_scale = 0             # Scale word size mainly by rank (0) to frequency (1) 
-    cloud_horizontal = 1        # Ratio of word angles from horizontal (1) to vertical (0) 
-    keys = tuple(df.index.tolist())
-    values = tuple(df['word_word'])
-    my_dict = dict(zip(keys,values))
 
-    wordcloud = WordCloud(regexp=None, relative_scaling=cloud_scale, prefer_horizontal=cloud_horizontal, width = 500 , height = 500, background_color=cloud_bg_color, max_words=500, contour_width=0, colormap = cloud_color)
 
-    wordcloud.generate_from_frequencies(my_dict)
+def tfidf_prep(df_list):
+    new_list = []
+    for document in df_list:
+        def make_bigrams(texts):
+            return [bigram_mod[doc] for doc in texts]
+        #prep stopwords
+        more_stopwords = ("de, much, dont, im, liking, getting, gotten, ever, always, every, many, few, even, got, get, make, made, let, makes, im, say, said, better, best, really, would, something, came,much, oh, put, also, lyrics, lyric, comment, comments, lot, 2020, 2019, 2018, hey, hi, may, making, many, cause,com, youtube, etc, else, since, comes, come, www, la,que, re, el, use, used, still, much, ever, every, around,also, da, finally, sure, literal, literally, ooh, ya, every, album, let, -PRON-, pron, http, https").split(",")
+        stops = list(stopwords.words('english'))
+        stops.extend(more_stopwords)
+        stops = [x.strip(' ') for x in stops]
 
-    return wordcloud
+        #extract comments
+        document = document.Comment.values.tolist()
+        #Remove mess
+        document = [re.sub('\S*@\S*\s?', '', sent) for sent in document]
+        # Remove new line characters
+        document = [re.sub('\s+', ' ', sent) for sent in document]
+        # Remove distracting single quotes
+        document = [re.sub("\'", "", sent) for sent in document]
+        #run tokenization:
+        document = list(sent_to_words(document))
+        #Run stopword removal:
+        document = [[word for word in simple_preprocess(str(doc)) if word not in set(stops)] for doc in document]
+        #Run lemmatization:
+    ###    document = lemmatization(document, allowed_postags=['NOUN', 'ADJ', 'VERB', 'ADV'])
+        #Remove weird tags:
+        document = [[word for word in simple_preprocess(str(doc)) if word not in set(stops)] for doc in document]
+        #Bigrams:
+        bigram = gensim.models.Phrases(document, min_count=10, threshold=30)
+        bigram_mod = gensim.models.phrases.Phraser(bigram)
+        document = make_bigrams(document)
+        #Smash to string:
+        document = [' '.join(sent) for sent in document]
+        document = ' '.join(document)
 
+        new_list.append(document)
+    return new_list
