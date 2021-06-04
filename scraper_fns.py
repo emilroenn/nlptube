6084fd198a88056004409bd9b5353461746d5f60
emilroenn <emilroenn@gmail.com>
2021-06-04 19:31:16 +0200

    bam

--- scraper_fns.py
+++ scraper_fns.py
@@ -3,10 +3,10 @@ import pickle
 import pandas as pd
 from pandas import DataFrame
 #import google.oauth2.credentials
-from googleapiclient.discovery import build
-from googleapiclient.errors import HttpError
-from google_auth_oauthlib.flow import InstalledAppFlow
-from google.auth.transport.requests import Request
+#from googleapiclient.discovery import build
+#from googleapiclient.errors import HttpError
+#from google_auth_oauthlib.flow import InstalledAppFlow
+#from google.auth.transport.requests import Request
 from youtube_transcript_api import YouTubeTranscriptApi
 import matplotlib.pyplot as plt
 import streamlit as st
